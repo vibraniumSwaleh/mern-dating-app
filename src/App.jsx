de42ff59212bc8f1ff21fbe2733eeaae803044c7
vibraniumSwaleh <hus.swaleh@yahoo.com>
2023-12-04 01:01:50 +0000

    set file types and structure app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles/App.css';
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+    return(<div className='app'>
+        <Header />
+    </div>);
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../styles/Header.css';
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
+import Logo192 from '../assets/logo192.png';
+
+const Header = () => {
+    return(<div className="header">
+        <IconButton>
+            <PersonIcon fontSize="large" className="header__icon"/>
+        </IconButton>
+        <img className="header__logo" src={Logo192} alt="header"/>
+        <IconButton>
+            <ForumIcon fontSize="large" className="header__icon"/>
+        </IconButton>
+    </div>)
+};
+
+export default Header;
